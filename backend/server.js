8f4492b0460afd6160b7e24ac525857483c63570
Ke Zhou <kezhou@Kes-MacBook-Pro.lan>
2024-01-31 21:39:14 -0500

    backend authentication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 // which is the same as: const express = require('express');
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoute.js';
+import userRoutes from './routes/userRoutes.js';
 
 connectDB();
 
@@ -12,11 +14,21 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
